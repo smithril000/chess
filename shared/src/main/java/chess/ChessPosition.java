f8ff3e15515b1ec0f0820e8ef82b6fef8ad4a03a
smithril000 <rileysmith1035@icloud.com>
2026-06-25 15:47:57 -0600

    Added most basic function for position

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,11 @@ package chess;
  */
 public class ChessPosition {
 
+    private int row;
+    private int col;
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,14 +20,14 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
-    }
+        return row;
+        }
 
     /**
      * @return which column this position is in
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
