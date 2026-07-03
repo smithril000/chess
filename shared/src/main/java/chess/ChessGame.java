9ef8e4cabf20fd7b4add9e75108285bce6c4487b
smithril000 <rileysmith1035@icloud.com>
2026-07-03 12:26:01 -0600

    Added test files

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,16 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private TeamColor teamTurn;
     public ChessGame() {
-
+        this.teamTurn = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return this.teamTurn;
     }
 
     /**
@@ -27,7 +28,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.teamTurn = team;
     }
 
     /**
