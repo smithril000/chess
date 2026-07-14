973fef73d3828a0a3e0c5e9f7305084b350390c5
smithril000 <rileysmith1035@icloud.com>
2026-07-14 16:35:48 -0600

    Most basic creating auth and applying it to memory works

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,15 +1,21 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
 import java.util.HashMap;
 
 //THIS IS TEMP FOR BEFORE DB
 public class MemoryDataAccess {
-    private static HashMap<String, UserData> userDataList = new HashMap<>();
+    private static final HashMap<String, UserData> userDataList = new HashMap<>();
+    private static final HashMap<String, AuthData> authDataList = new HashMap<>();
 
     public static void createUserDate(UserData user) {
         userDataList.put(user.username(), user);
     }
 
+
+    public static void createAuthData(AuthData authData) {
+        authDataList.put(authData.authToken(), authData);
+    }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -2,7 +2,10 @@ package service;
 
 import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import model.AuthData;
 import model.UserData;
+
+import java.util.UUID;
 
 public class UserService {
     public UserService(){
@@ -10,6 +13,16 @@ public class UserService {
     }
 
     public static void register(UserData user) throws DataAccessException {
+        //we need to creat both the userdata and the authdata
         MemoryDataAccess.createUserDate(user);
+        //create the auth data
+        String auth = generateToken();
+        //we can create the authData here
+        AuthData authData = new AuthData(auth, user.username());
+        MemoryDataAccess.createAuthData(authData);
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 }
