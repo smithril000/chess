a36b2e6bf9c4f11edb04dba3ff17edcc0ad5531c
smithril000 <rileysmith1035@icloud.com>
2026-07-21 08:46:37 -0600

    added some unit service tests

---
+++ server/src/test/java/dataaccess/DatabaseTests.java
@@ -0,0 +1,58 @@
+package dataaccess;
+
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+
+public class DatabaseTests {
+    UserData testUser = new UserData("testUser", "testEmail", "testPass");
+    //UserData testUser2 = new UserData("testUser2", "testEmail2", "testPass2");
+
+    @BeforeEach
+    void clean() throws ResponseException {
+        UserService.clear();
+    }
+    @Test
+    void registerSuccess() throws ResponseException {
+        var response = UserService.register(testUser);
+        Assertions.assertEquals("testUser", response.username());
+    }
+
+    @Test
+    void registerTwice() throws ResponseException {
+        UserService.register(testUser);
+        try {
+            UserService.register(testUser);
+        }catch(ResponseException ex){
+            Assertions.assertEquals(500, ex.getCode());
+        }
+
+    }
+
+    @Test
+    void loginSuccess() throws ResponseException {
+        var response = UserService.register(testUser);
+        UserService.logout(response.authToken());
+        //now login
+        response = UserService.login(testUser);
+
+        Assertions.assertEquals("testUser",response.username());
+    }
+
+    @Test
+    void loginNotAllowed() {
+        try{
+            UserService.login(testUser);
+        } catch (ResponseException ex) {
+            Assertions.assertEquals(401, ex.getCode());
+        }
+    }
+
+    @Test
+    void logoutSuccess() throws ResponseException {
+        var auth = UserService.register(testUser).authToken();
+        UserService.logout(auth);
+    }
+}
