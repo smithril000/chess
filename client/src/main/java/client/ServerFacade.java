c68e1f3d9f2feb2a3ff640612c040f17a4adc04f
smithril000 <rileysmith1035@icloud.com>
2026-07-28 09:29:45 -0600

    started first iter of register for my server

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,6 +1,12 @@
 package client;
 
+import com.google.gson.Gson;
+import dataaccess.ResponseException;
+
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Map;
 
 public class ServerFacade {
@@ -14,6 +20,32 @@ public class ServerFacade {
     public void register(Map<String, String> data){
         //we need to make what we pu tin look like the web ui
         //map should work
+        var request = buildRequest("POST", "/user", data, null);
+        var response = sendRequest(request);
+    }
+    private HttpRequest buildRequest(String method, String path, Object body, String authToken){
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (authToken != null) {
+            request.setHeader("authorization", authToken);
+        }
+        return request.build();
+    }
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+
 
     }
 }
