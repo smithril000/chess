e0f8427ff9b96a2a20e70a84546f049e2393b818
rileysmith <rileysmith1035@icloud.com>
2026-08-04 12:11:33 -0600

    did starter code, and starting ws facade

---
+++ client/src/main/java/client/websocket/ClientWebSocket.java
@@ -0,0 +1,46 @@
+package client.websocket;
+
+import com.google.gson.Gson;
+
+import model.ResponseException;
+
+import jakarta.websocket.*;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ClientWebSocket extends Endpoint{
+    Session session;
+    ServerMessage message;
+
+    public ClientWebSocket(String url, ServerMessage notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.message = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify();
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(400, ex.getMessage());
+        }
+    }
+
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+
+}
